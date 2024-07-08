fbd7d449498ce212c4aa7906c70eb1365cd63a98
zian17 <zian.zcmu120@gmail.com>
2024-07-07 21:48:34 -0600

    Cambios en cursos 3

--- edumocs/cursos/admin.py
+++ edumocs/cursos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Cursos
 
-# Register your models here.
+
+class AdministrarCurso(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+    list_display = ('id','nombre','profesor')
+    search_fields =('id','nombre','profesor')
+    date_hierarchy = 'created'
+    list_filter = ('profesor','fecha_inicio','fecha_termino')
+
+admin.site.register(Cursos,AdministrarCurso)
