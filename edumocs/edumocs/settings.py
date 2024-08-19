4670aa190a90b70de18defa49c86c8c458130a16
JonyOz <jonyh2oz@gmail.com>
2024-08-19 14:27:34 -0600

    Ckeditor implementado

--- edumocs/edumocs/settings.py
+++ edumocs/edumocs/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'administrador',
     'cursos.apps.CursosConfig',
     'foro',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
+CKEDITOR_CONFIGS ={
+'default':{
+'toolbar':'Basic',
+}
+}
+
+CKEDITOR_CONFIGS = {
+
+'default': {'toolbar': 'Custom',
+'toolbar_Custom': [
+['Bold', 'Italic', 'Underline'],
+['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-',
+'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+['Link', 'Unlink'],
+['RemoveFormat', 'Source']
+]
+}
+}
